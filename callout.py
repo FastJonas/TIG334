6bc7b780c026342e030bc08f8e43ac815f8e429c
Jonas <99483020+FastJonas@users.noreply.github.com>
2024-03-29 10:04:40 +0100

    Update callout.py

--- callout.py
+++ callout.py
@@ -1,19 +1,14 @@
 from twilio.rest import Client
 
-# Twilio Account SID and Auth Token
 account_sid = "AC9ce42e4aa45fc638ad53e11d208af9l"
 auth_token = "1c5608e7bce2965cde9ec59eac35880d"
 
-# Your Twilio phone number
 twilio_number = '+15642323047'
 
-# Your phone number
 your_number = '+46723000510'
 
-# Initialize Twilio Client
 client = Client(account_sid, auth_token)
 
-# Make a call
 call = client.calls.create(
     to=your_number,
     from_=twilio_number,
